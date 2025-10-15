3f93521b67fcc4847366683275db187e44e2e9b9
Johnfonx8 <146359517+Johnfonx8@users.noreply.github.com>
2025-10-15 13:50:01 +0700

    add 1st commit

--- tools/ci/npc.sh
+++ tools/ci/npc.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-out=npc/scripts_custom.conf
+out=npcTH/scripts_custom.conf
 
 printf "\n" >> $out
 echo "// Custom Scripts" >> $out
 
-find npc/custom \( -name "*.txt" \) | sort | xargs -I % echo "npc: %" >> $out
+find npcTH/custom \( -name "*.txt" \) | sort | xargs -I % echo "npc: %" >> $out
 
 echo "// Test Scripts" >> $out
 
-find npc/test \( -name "*.txt" \) | sort | xargs -I % echo "npc: %" >> $out
+find npcTH/test \( -name "*.txt" \) | sort | xargs -I % echo "npc: %" >> $out
